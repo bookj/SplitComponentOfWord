153f8c54f19370edc2c8b332d8c6b323dfbb6f6a
XezXey <puntawat-1@hotmail.com>
2016-09-06 22:59:14 +0700

    Can split and strip non-thai character

--- tests/analyzers/test_meter.py
+++ tests/analyzers/test_meter.py
@@ -1,38 +1,78 @@
 import unittest
 from thws.analyzers import meter
 
+class SeparateWords(unittest.TestCase):
+
+    def setUp(self):
+        self.az = meter.MeterAnalyzer()
+
+    def test_given_sunthorn_phu_sentence_1(self):
+        sentence = ("ถึงม้วยดินสิ้นฟ้ามหาสมุทร")
+        output = ["ถึง", "ม้วย", "ดิน", "สิ้น", "ฟ้า", "มหาสมุทร"]
+
+        result = self.az.separate_word(sentence)
+        self.assertEqual(result, output)
+
+    def test_given_sunthorn_phu_sentence_2(self):
+        sentence = ("ไม่สิ้นสุดความรักสมัครสมาน")
+        output = ["ไม่", "สิ้น", "สุด", "ความ", "รัก", "สมัคร", "สมาน"]
+
+        result = self.az.separate_word(sentence)
+        self.assertEqual(result, output)
+
+
 class StripingSpecialCharacterTest(unittest.TestCase):
 
     def setUp(self):
         self.az = meter.MeterAnalyzer()
 
     def test_given_words_should_strip_exclamation(self):
-        words = ['การ', '!']
-        output = ['การ']
+        words = ('การ!')
+        output = ('การ')
 
         result = self.az.strip_special_character(words)
         self.assertEqual(result, output)
 
     def test_given_word_should_strip_question_mark(self):
-        word = ['ถึง', 'ม้วย', 'ดิน', 'สิ้น', 'ฟ้า', 'มหาสมุทร', '?']
-        output = ['ถึง', 'ม้วย', 'ดิน', 'สิ้น', 'ฟ้า', 'มหาสมุทร']
+        word = ("ถึงม้วยดินสิ้นฟ้ามหาสมุทร์?")
+        output = ("ถึงม้วยดินสิ้นฟ้ามหาสมุทร์")
 
         result = self.az.strip_special_character(word)
         self.assertEqual(result, output)
 
     def test_given_word_should_strip_space(self):
-        word = ['ไม่', 'สิ้น', ' ', 'สุด', 'ความ', ' ', 'รัก', 'สมัคร', 'สมาน']
-        output = ['ไม่', 'สิ้น', 'สุด', 'ความ', 'รัก', 'สมัคร', 'สมาน']
+        word = ("ไม่สิ้นสุด ความรัก สมัครสมาน")
+        output = ("ไม่สิ้นสุดความรักสมัครสมาน")
 
         result = self.az.strip_special_character(word)
         self.assertEqual(result, output)
 
 
     def test_given_word_should_strip_english_alphabet(self):
-        word = ['ไม่', 'สิ้น', 'สุดalwaysความ', 'รักasdfwสมัครgoneสมาน']
-        output = ['ไม่', 'สิ้น', 'สุด', 'ความ', 'รัก', 'สมัคร', 'สมาน']
+        word = ("ไม่สิ้นสุดalwaysความรักasdfwสมัครgoneสมาน")
+        output = ("ไม่สิ้นสุดความรักสมัครสมาน")
 
         result = self.az.strip_special_character(word)
         self.assertEqual(result, output)
 
+    def test_given_word_should_strip_new_line(self):
+        word = ("บัตรอนุญาติเข้าห้อง\nปฏิบัติการ")
+        output = ("บัตรอนุญาติเข้าห้องปฏิบัติการ")
+
+        result = self.az.strip_special_character(word)
+        self.assertEqual(result, output)
+
+    def test_given_word_should_strip_tab(self):
+        word = ("บัตรอนุญาติเข้าห้อง\tปฏิบัติ\tการ")
+        output = ("บัตรอนุญาติเข้าห้องปฏิบัติการ")
+
+        result = self.az.strip_special_character(word)
+        self.assertEqual(result, output)
+
+    def test_given_word_should_strip_full_stop(self):
+        word = ("บัตรอนุญาติเข้าห้องปฏิบัติการ.")
+        output = ("บัตรอนุญาติเข้าห้องปฏิบัติการ")
+
+        result = self.az.strip_special_character(word)
+        self.assertEqual(result, output)
 
--- thws/analyzers/meter.py
+++ thws/analyzers/meter.py
@@ -8,20 +8,19 @@ class MeterAnalyzer:
         self.special_character = r"[^ก-ฮ\u0E2F-\u0E3A\u0E40-\u0E5B]"
 
     def process(self, text):
-        words = self.separate_word(text)
-        self.words = strip_special_character(words)
+        words = strip_special_character(text)
+        self.words = self.separate_word(words)
         return self.words
 
     def strip_special_character(self, words):
-        new_words = []
-        for word in words:
-            if not re.search(self.special_character, word):
-                new_words.append(word)
-            else if:
-                
+        new_words = ''
+        new_words_list = re.split(self.special_character, words)
+        for word in new_words_list:
+            if word != '':
+                new_words = new_words + word
         return new_words
 
-    def separate_word(self, word):
+    def separate_word(self, text):
         boundary = PyICU.BreakIterator.createWordInstance(PyICU.Locale("th"))
         boundary.setText(text)
         last_position = boundary.first()
